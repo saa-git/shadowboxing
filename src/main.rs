0ffab73000b2dfd53c009b67da4bd50f17b340d1
Sharif Abdullahi <55369861+saa-git@users.noreply.github.com>
2023-05-01 07:54:01 -0400

    LICENSE

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 // Copyright 2023 Sharif Abdullahi.
 // Use of this source code is governed by The Unlicense
 // which can be found in the LICENSE file.
+
+
+//
 use std::{
     io::{stdout, Write},
     thread,
